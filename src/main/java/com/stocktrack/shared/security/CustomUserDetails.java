2e3442d1ec6b59403360c9b1b67c60e57be8c010
Breno <breno.joaosalvadorperez200@gmail.com>
2026-08-10 09:12:39 -0300

    feat: create CustomUserDetails and CustomUserDetailsService

---
+++ src/main/java/com/stocktrack/shared/security/CustomUserDetails.java
@@ -0,0 +1,58 @@
+package com.stocktrack.shared.security;
+
+import com.stocktrack.user.entity.User;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final com.stocktrack.user.entity.User user;
+
+    public CustomUserDetails(com.stocktrack.user.entity.User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getRe();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/stocktrack/shared/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.stocktrack.shared.security;
+
+import com.stocktrack.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String re) throws UsernameNotFoundException {
+        return userRepository.findByRe(re)
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com RE: " + re));
+    }
+}

8d4da389d5235fdc68e097fe2d409d32ef4826e3
Breno <breno.joaosalvadorperez200@gmail.com>
2026-08-11 11:01:26 -0300

    feat: create ToolTypeMapper with category collection mapping

---
+++ src/main/java/com/stocktrack/tooltype/mapper/ToolTypeMapper.java
@@ -0,0 +1,32 @@
+package com.stocktrack.tooltype.mapper;
+
+import com.stocktrack.category.entity.Category;
+import com.stocktrack.tooltype.dto.request.ToolTypeRequestDTO;
+import com.stocktrack.tooltype.dto.response.ToolTypeResponseDTO;
+import com.stocktrack.tooltype.dto.response.ToolTypeResponseDTO.CategorySummaryDTO;
+import com.stocktrack.tooltype.entity.ToolType;
+import org.mapstruct.*;
+
+@Mapper(
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.ERROR
+)
+public interface ToolTypeMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "categories", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    ToolType toEntity(ToolTypeRequestDTO dto);
+
+    @Mapping(target = "availableQuantity", source = "availableQuantity")
+    ToolTypeResponseDTO toResponse(ToolType toolType, int availableQuantity);
+
+    CategorySummaryDTO toCategorySummaryDto(Category category);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "categories", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    void updateEntityFromDto(ToolTypeRequestDTO dto, @MappingTarget ToolType toolType);
+}
